24ab5589b70091520f02d9ee9ea1efcfebf4f908
SuprathikJoshua <suprathikj@gmail.com>
2025-12-27 21:09:19 +0530

    Add user model, API error and response classes, and async handler utility

---
+++ packages/Backend/src/models/user.models.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: ["OWNER", "ADMIN", "MEMBER"],
+    default: "MEMBER",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
